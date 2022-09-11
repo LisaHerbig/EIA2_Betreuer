8aefdf4cabe95eebdb1835bb03e0e140d413132f
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2022-09-11 17:57:19 +0200

    L06_DBS

--- WiSe22_23/L06/L06Script.js
+++ WiSe22_23/L06/L06Script.js
@@ -3,11 +3,11 @@ var L06_Einkaufsliste;
 (function (L06_Einkaufsliste) {
     window.addEventListener("load", handleLoad);
     let allEntries = [];
-    let names = [];
-    let counts = [];
-    let comments = [];
+    //let names: string[] = [];
+    //let counts: string[] = [];
+    //let comments: string[] = [];
     let form;
-    let url = "https://webuser.hs-furtwangen.de/~herbigli/L06_Data";
+    let url = "https://webuser.hs-furtwangen.de/~herbigli/L06_Data/";
     async function handleLoad(_event) {
         form = document.querySelector("#form");
         let submit = document.querySelector("#add");
@@ -18,11 +18,10 @@ var L06_Einkaufsliste;
         L06_Einkaufsliste.generateContent(data);
         let button = document.querySelector("#add");
         button.addEventListener("pointerdown", displayList);
-        addData(data);
+        addData();
     }
     async function sendEntry() {
         let form = document.querySelector("#form");
-        //let formData: FormData = new FormData(form);
         let formData = new FormData(form);
         let json = {};
         for (let key of formData.keys())
@@ -30,9 +29,6 @@ var L06_Einkaufsliste;
                 let values = formData.getAll(key);
                 json[key] = values.length > 1 ? values : values[0];
             }
-        //let query: URLSearchParams = new URLSearchParams(<any>formData);
-        //await fetch("L06.html?" + query.toString());
-        //alert("Entry added :)");
         let query = new URLSearchParams();
         query.set("command", "insert");
         query.set("collection", "Entries");
@@ -40,46 +36,109 @@ var L06_Einkaufsliste;
         console.log(query.toString() + "     queryToString");
         let response = await fetch(url + "?" + query.toString());
         let responseText = await response.text();
-        alert(responseText);
+        alert(responseText + " :)");
     }
     function displayList() {
-        //console.log("displayList");
-        let formData = new FormData(form);
-        let newEntry = document.createElement("div");
-        for (let entry of formData) {
-            let entryName;
-            let entryCount;
-            let entryComment;
+        addData();
+        //let formData: FormData = new FormData(form);
+        //let newEntry: HTMLElement = document.createElement("div");
+        /*for (let entry of formData) {
+            let entryName: string;
+            let entryCount: string;
+            let entryComment: string;
             switch (entry[0]) {
                 case "name":
-                    console.log("CaseName" + entry[1]);
                     entryName = String(entry[1]);
                     newEntry.innerText = entryName + "\n";
                     break;
                 case "count":
-                    console.log("CaseCount" + entry[1]);
                     entryCount = String(entry[1]);
                     newEntry.innerText += " " + entryCount + "\n";
                     break;
                 case "comment":
-                    console.log(("CaseComment" + entry[1]));
                     entryComment = String(entry[1]);
                     newEntry.innerText += " " + entryComment + "\n";
                     break;
                 default:
                     console.log("Default");
             }
-            let list = document.querySelector("#list");
+            let list: HTMLElement = <HTMLElement>document.querySelector("#list");
             list.appendChild(newEntry);
         }
+        let entryDate: string;
+        let date: Date = new Date;
+        let day: number = date.getDay();
+        let month: number = date.getMonth();
+        let year: number = date.getFullYear();
+        entryDate = day + "/" +  month + "/" + year;
+        newEntry.innerHTML += "" + entryDate;
+        let radio: HTMLInputElement = document.createElement("input");
+        radio.type = "radio";
+        radio.setAttribute("class", "center");
+        newEntry.appendChild(radio);
+        let trash: HTMLElement = document.createElement("i");
+        trash.setAttribute("class", "fa-solid fa-trash-can");
+        trash.setAttribute("id", "delete");
+        trash.addEventListener("pointerdown", handleDelete);
+        newEntry.appendChild(trash);
+        allEntries.push(newEntry);
+        let list: HTMLElement = <HTMLElement>document.querySelector("#list");
+        list.appendChild(newEntry);*/
+    }
+    async function addData() {
+        let query = new URLSearchParams();
+        query.set("command", "find");
+        query.set("collection", "Entries");
+        query.toString();
+        console.log(query + " queryCommandFind");
+        let response = await fetch(url + "?" + query.toString());
+        let responseText = await response.text();
+        //alert(responseText + " :))");
+        /*for (let category in _data) {
+            let item: Item[] = _data[category];
+            switch (category) {
+                case "name":
+                     pushNames(item);
+                     break;
+                case "count":
+                    pushCount(item);
+                    break;
+                case "comment":
+                pushComment(item);
+                default:
+                    break;
+            }
+        }*/
+        buildList(responseText);
+    }
+    /*function pushNames(_item: Item[]): void {
+        for (let entry of _item) {
+            names.push(entry.name);
+            //console.log("EntryName " + entry.name + "namesArray " + names);
+        }
+    }
+
+    function pushCount(_item: Item[]): void {
+        for (let entry of _item) {
+            counts.push(entry.name);
+        }
+    }
+
+    function pushComment(_item: Item[]): void {
+        for (let entry of _item) {
+            comments.push(entry.name);
+        }
+    }*/
+    function buildList(_responseText) {
+        let list = document.querySelector("#list");
         let entryDate;
         let date = new Date;
         let day = date.getDay();
         let month = date.getMonth();
         let year = date.getFullYear();
         entryDate = day + "/" + month + "/" + year;
-        newEntry.innerHTML += "" + entryDate;
-        console.log(newEntry);
+        let newEntry = document.createElement("div");
+        newEntry.innerText = _responseText;
         let radio = document.createElement("input");
         radio.type = "radio";
         radio.setAttribute("class", "center");
@@ -89,72 +148,23 @@ var L06_Einkaufsliste;
         trash.setAttribute("id", "delete");
         trash.addEventListener("pointerdown", handleDelete);
         newEntry.appendChild(trash);
-        allEntries.push(newEntry);
-        let list = document.querySelector("#list");
         list.appendChild(newEntry);
-    }
-    function addData(_data) {
-        let query = new URLSearchParams();
-        query.set("command", "find");
-        query.toString();
-        console.log(query + "queryCommandFind");
-        for (let category in _data) {
-            let item = _data[category];
-            switch (category) {
-                case "name":
-                    pushNames(item);
-                    break;
-                case "count":
-                    pushCount(item);
-                    break;
-                case "comment":
-                    pushComment(item);
-                default:
-                    break;
-            }
-        }
-        buildList();
-    }
-    function pushNames(_item) {
-        for (let entry of _item) {
-            names.push(entry.name);
-            //console.log("EntryName " + entry.name + "namesArray " + names);
-        }
-    }
-    function pushCount(_item) {
-        for (let entry of _item) {
-            counts.push(entry.name);
-        }
-    }
-    function pushComment(_item) {
-        for (let entry of _item) {
-            comments.push(entry.name);
-        }
-    }
-    function buildList() {
-        let list = document.querySelector("#list");
-        let entryDate;
-        let date = new Date;
-        let day = date.getDay();
-        let month = date.getMonth();
-        let year = date.getFullYear();
-        entryDate = day + "/" + month + "/" + year;
         //console.log(names + " " + counts + " " + comments + " Arrays");
-        for (let index = 0; index <= names.length - 1; index++) {
+        /*for (let index: number = 0; index <= names.length - 1; index++) {
             //console.log(index + " index");
-            let newEntry = document.createElement("div");
+            let newEntry: HTMLElement = document.createElement("div");
             newEntry.innerText = names[index] + " " + counts[index] + " " + comments[index] + " " + entryDate;
-            let radio = document.createElement("input");
+            let radio: HTMLInputElement = document.createElement("input");
             radio.type = "radio";
             radio.setAttribute("class", "center");
             newEntry.appendChild(radio);
-            let trash = document.createElement("i");
+            let trash: HTMLElement = document.createElement("i");
             trash.setAttribute("class", "fa-solid fa-trash-can");
             trash.setAttribute("id", "delete");
             trash.addEventListener("pointerdown", handleDelete);
             newEntry.appendChild(trash);
             list.appendChild(newEntry);
-        }
+        }*/
     }
     function handleDelete(_event) {
         //console.log("delete");
--- WiSe22_23/L06/L06Script.ts
+++ WiSe22_23/L06/L06Script.ts
@@ -1,13 +1,14 @@
 namespace L06_Einkaufsliste {
+    
     window.addEventListener("load", handleLoad);
 
     let allEntries: HTMLElement[] = [];
-    let names: string[] = [];
-    let counts: string[] = [];
-    let comments: string[] = [];
+    //let names: string[] = [];
+    //let counts: string[] = [];
+    //let comments: string[] = [];
     
     let form: HTMLFormElement;
-    let url: string = "https://webuser.hs-furtwangen.de/~herbigli/L06_Data";
+    let url: string = "https://webuser.hs-furtwangen.de/~herbigli/L06_Data/";
 
     interface FormDataJSON {
         [key: string]: FormDataEntryValue | FormDataEntryValue[];
@@ -31,13 +32,12 @@ namespace L06_Einkaufsliste {
         let button: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#add");
         button.addEventListener("pointerdown", displayList);
 
-        addData(data);
+        addData();
     }
 
     async function sendEntry(): Promise<void> {
         
         let form: HTMLFormElement = <HTMLFormElement>document.querySelector("#form");
-        //let formData: FormData = new FormData(form);
         let formData: FormData = new FormData(form);
         let json: FormDataJSON = {};
 
@@ -48,10 +48,6 @@ namespace L06_Einkaufsliste {
         
         }
 
-        //let query: URLSearchParams = new URLSearchParams(<any>formData);
-        //await fetch("L06.html?" + query.toString());
-        //alert("Entry added :)");
-
         let query: URLSearchParams = new URLSearchParams();
         query.set("command", "insert");
         query.set("collection", "Entries");
@@ -61,52 +57,38 @@ namespace L06_Einkaufsliste {
 
         let response: Response = await fetch(url + "?" + query.toString());
         let responseText: string = await response.text();
-        alert(responseText);
+        alert(responseText + " :)");
         
         
     }
 
     function displayList(): void {
-
-        //console.log("displayList");
-        
-        let formData: FormData = new FormData(form);
-        let newEntry: HTMLElement = document.createElement("div");
-        
-        for (let entry of formData) {
-
+        addData();
+        //let formData: FormData = new FormData(form);
+        //let newEntry: HTMLElement = document.createElement("div");
+        /*for (let entry of formData) {
             let entryName: string;
             let entryCount: string; 
             let entryComment: string; 
-
             switch (entry[0]) {
                 case "name":
-                    console.log("CaseName" + entry[1]);
                     entryName = String(entry[1]);
                     newEntry.innerText = entryName + "\n";
                     break;
-
                 case "count":
-                    console.log("CaseCount" + entry[1]);
                     entryCount = String(entry[1]);
                     newEntry.innerText += " " + entryCount + "\n";
                     break;
-
-                case "comment":
-                    console.log(("CaseComment" + entry[1]));
+                case "comment":                  
                     entryComment = String(entry[1]);
                     newEntry.innerText += " " + entryComment + "\n";
                     break;
-
                 default:
-                    console.log("Default");
-                    
+                    console.log("Default");       
             }
-
             let list: HTMLElement = <HTMLElement>document.querySelector("#list");
             list.appendChild(newEntry);
         }
-
         let entryDate: string;
         let date: Date = new Date;
         let day: number = date.getDay();
@@ -114,67 +96,52 @@ namespace L06_Einkaufsliste {
         let year: number = date.getFullYear();
         entryDate = day + "/" +  month + "/" + year;
         newEntry.innerHTML += "" + entryDate;
-        console.log(newEntry);
-
         let radio: HTMLInputElement = document.createElement("input");
         radio.type = "radio";
         radio.setAttribute("class", "center");
         newEntry.appendChild(radio);
-
         let trash: HTMLElement = document.createElement("i");
         trash.setAttribute("class", "fa-solid fa-trash-can");
         trash.setAttribute("id", "delete");
         trash.addEventListener("pointerdown", handleDelete);
         newEntry.appendChild(trash);
-
         allEntries.push(newEntry);
-
         let list: HTMLElement = <HTMLElement>document.querySelector("#list");
-        list.appendChild(newEntry);
+        list.appendChild(newEntry);*/
     }
 
-    function addData(_data: Data): void {
+    async function addData(): Promise<void> {
         
         let query: URLSearchParams = new URLSearchParams();
         query.set("command", "find");
+        query.set("collection", "Entries");
         query.toString();
-        console.log(query + "queryCommandFind");
-        
-        for (let category in _data) {
-
+        console.log(query + " queryCommandFind");
+        let response: Response = await fetch(url + "?" + query.toString());
+        let responseText: string = await response.text();
+        //alert(responseText + " :))");
+        /*for (let category in _data) {
             let item: Item[] = _data[category];
-
             switch (category) {
-
                 case "name":
-
                      pushNames(item);
-
                      break;
-
                 case "count":
-
                     pushCount(item);
-
                     break;
-
                 case "comment":
-
-                pushComment(item);
-            
+                pushComment(item);    
                 default:
                     break;
             }
-        }
-
-        buildList();
+        }*/
+        buildList(responseText);
     }
 
-    function pushNames(_item: Item[]): void {
+    /*function pushNames(_item: Item[]): void {
         for (let entry of _item) {
             names.push(entry.name);
-            //console.log("EntryName " + entry.name + "namesArray " + names);
-            
+            //console.log("EntryName " + entry.name + "namesArray " + names);        
         }
     }
 
@@ -188,9 +155,9 @@ namespace L06_Einkaufsliste {
         for (let entry of _item) {
             comments.push(entry.name);
         }
-    }
+    }*/
 
-    function buildList(): void {
+    function buildList(_responseText: string): void {
         
         let list: HTMLElement = <HTMLElement>document.querySelector("#list");
 
@@ -200,32 +167,38 @@ namespace L06_Einkaufsliste {
         let month: number = date.getMonth();
         let year: number = date.getFullYear();
         entryDate = day + "/" +  month + "/" + year;
-        
+        let newEntry: HTMLDivElement = document.createElement("div");
+        newEntry.innerText = _responseText;
+
+        let radio: HTMLInputElement = document.createElement("input");
+        radio.type = "radio";
+        radio.setAttribute("class", "center");
+        newEntry.appendChild(radio);
+
+        let trash: HTMLElement = document.createElement("i");
+        trash.setAttribute("class", "fa-solid fa-trash-can");
+        trash.setAttribute("id", "delete");
+        trash.addEventListener("pointerdown", handleDelete);
+        newEntry.appendChild(trash);
+
+        list.appendChild(newEntry);
 
         //console.log(names + " " + counts + " " + comments + " Arrays");
-        
-
-        for (let index: number = 0; index <= names.length - 1; index++) {
-
+        /*for (let index: number = 0; index <= names.length - 1; index++) {
             //console.log(index + " index");
-            
             let newEntry: HTMLElement = document.createElement("div");
             newEntry.innerText = names[index] + " " + counts[index] + " " + comments[index] + " " + entryDate;
-
             let radio: HTMLInputElement = document.createElement("input");
             radio.type = "radio";
             radio.setAttribute("class", "center");
             newEntry.appendChild(radio);
-
             let trash: HTMLElement = document.createElement("i");
             trash.setAttribute("class", "fa-solid fa-trash-can");
             trash.setAttribute("id", "delete");
             trash.addEventListener("pointerdown", handleDelete);
             newEntry.appendChild(trash);
-
             list.appendChild(newEntry);
-          
-        }
+        }*/
 
     }
 
